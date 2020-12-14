6c36ca4620bccd1789ed9bd6326b7c722b9de921
rahathosen <rahathosen.me@gmail.com>
2020-12-14 19:31:53 +0600

    Updates

---
+++ precache-manifest.08bc49f2dafe546273e7e4bd08a597b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1133e321ab2fa1582d598e975797d93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9fd34725ec3a700894b",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9fd34725ec3a700894b",
+    "url": "/static/js/main.d189c9b6.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "c59da1ab0b28f10bc7858a3bdaa702ea",
+    "url": "/static/media/rahathosen.c59da1ab.jpg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/resume.d41d8cd9.pdf"
+  }
+]);
